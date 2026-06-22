d0c06ba131e10b43b421e67586686ab373f4b39b
Purushothaman S <purush102005@gmail.com>
2026-06-22 19:47:31 +0530

    Add files via upload

--- mom-frontend/src/App.js
+++ mom-frontend/src/App.js
@@ -3,12 +3,14 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Meetings from "./pages/Meetings";
 import Tasks from "./pages/Tasks";
 import Settings from "./pages/Settings";
 import ViewMeeting from "./pages/ViewMeeting";
+import Extension from "./pages/Extension";
+
 
 function App() {
 
@@ -16,14 +18,17 @@ function App() {
     <BrowserRouter>
 
       <Routes>
+        <Route path="/"  element={<Login />} />
 
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         <Route path="/meetings" element={<Meetings />} />
 
         <Route path="/tasks" element={<Tasks />} />
 
         <Route path="/settings" element={<Settings />} />
+        
+        <Route path="/extension" element={<Extension />} />
 
         <Route path="/meetings/:id" element={<ViewMeeting />} />
 
