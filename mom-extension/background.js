a42bca336079e4ac8bf609592738ee7151756374
NITHIYA SRI D <nithiyasri.d.2023.cse@ritchennai.edu.in>
2026-06-29 12:28:54 +0530

    Prepare extension architecture for tab capture

--- mom-extension/background.js
+++ mom-extension/background.js
@@ -1,13 +1,24 @@
-// background.js
-
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("MoM Recorder Extension Installed");
+    console.log("MoM Extension Installed");
 });
 
 chrome.runtime.onStartup.addListener(() => {
-    console.log("MoM Recorder Started");
+    console.log("MoM Extension Started");
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
     console.log("Message received:", message);
+
+    if (message.action === "start-recording") {
+        console.log("Start recording requested");
+    }
+
+    if (message.action === "stop-recording") {
+        console.log("Stop recording requested");
+    }
+
+    sendResponse({ success: true });
+
+    return true;
 });
